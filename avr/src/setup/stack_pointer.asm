fbfb27527b8a2b368dcf65401a5bb51ac137508b
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2016-03-07 22:52:25 +0100

    engelsk -> dansk

--- avr/src/setup/stack_pointer.asm
+++ avr/src/setup/stack_pointer.asm
@@ -1,4 +1,4 @@
-;initialize sp, so we can make subroutines and interrupts
+; Initialiser stack pointer, så vi kan bruge "call / callr", lave interrupts mm.
 ldi R16, HIGH(RAMEND)
 out SPH, R16
 ldi R16, LOW(RAMEND)
