956c4c98cbb28af77f75a51e9771c518dd8f5742
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2016-03-29 23:21:49 +0200

    Preprocessor

---
+++ avr/preprocessor/preprocessor.js
@@ -0,0 +1,105 @@
+"use strict";
+
+var t0 = new Date();
+
+const fs = require('fs');
+const escapeRgx = require('escape-string-regexp');
+const rimraf = require('rimraf');
+
+var outputDir = './temp';
+var inputDir = './src';
+
+rimraf.sync(outputDir);
+
+var files = [];
+var readPath = (path) => {
+	var stats = fs.statSync(path);
+	if (stats.isFile()){
+		var fileext = path.substr(-4).toUpperCase();
+		if (fileext === '.ASM' || fileext === '.INC') files.push(path);
+	}else if (stats.isDirectory()){
+		fs.mkdirSync(path.replace(inputDir, outputDir));
+
+		fs.readdirSync(path).forEach(subPath => {
+			readPath(path + '/' + subPath);
+		});
+	}else{
+		console.log(path + ", is not a file nor a path?");
+	}
+}
+readPath(inputDir);
+
+const RGX_D = (directive, seperator) => {
+	return new RegExp(
+		'^[\\t ]*\\.' + escapeRgx(directive) +
+		'[\\t ]+([^\\t\\n\\r= ]*)\\s*' + escapeRgx(seperator) + '\\s*([^\\t\\n\\r; ]*)'
+		,	'gmi'
+	);
+}
+
+const lineErr = (filePath, index) => {
+	console.log(filePath + '(' + (index + 1) + ')');
+}
+
+var rgxFileDef = RGX_D("filedef", "=");
+
+var processFile = (inputPath, outputPath) => {
+	var text = fs.readFileSync(inputPath, "utf8");
+	var lines = text.split('\n');
+	var fileDefs = {names:[], values:[]};
+
+	lines.forEach((line, i) => {
+		rgxFileDef.lastIndex = 0;
+		var m = rgxFileDef.exec(line);
+		if (!m) return;
+		if (m.length !== 3 || m[1].length === 0 || m[2].length === 0){
+				lineErr(filePath, i);
+				console.log('Empty .filedef name or value!');
+				process.exit(1);
+		}
+		var name = m[1].toUpperCase();
+		var nameIndex = fileDefs.names.indexOf(name);
+		if(nameIndex !== -1){
+			lineErr(filePath, i);
+			console.log('Name "' + name + '" already defined to value "' + fileDefs.values[nameIndex] + '"');
+			process.exit(1);
+		}
+		var value = m[2].toUpperCase();
+		var valueIndex = fileDefs.values.indexOf(value);
+		if(valueIndex !== -1){
+			lineErr(filePath, i);
+			console.log('Value "' + value + '" already defined to name "' + fileDefs.names[valueIndex] + '"');
+			process.exit(1);
+		}
+		fileDefs.names.push(name);
+		fileDefs.values.push(value);
+
+		lines[i] = ';' + line;
+	});
+
+	for (var i = 0; i < fileDefs.names.length; i++){
+		var name = fileDefs.names[i];
+		var value = fileDefs.values[i];
+
+		var nameRgx = new RegExp('\\b' + escapeRgx(name) + '\\b','gi');
+
+		lines.forEach((line, i) => {
+			var lineparts = line.split(';');
+			lineparts[0] = lineparts[0].replace(nameRgx, value);
+			lines[i] = lineparts.join(';');
+		});
+	}
+
+	var newText = lines.join('\n');
+	fs.writeFileSync(outputPath, newText);
+}
+
+files.forEach(path => {
+	processFile(path, path.replace(inputDir, outputDir));
+});
+
+var d1 = new Date();
+
+console.log("Processed in " + (d1 - d0) + ' ms');
+
+process.exit(0);
--- avr/src/program.asm
+++ avr/src/program.asm
@@ -1,1 +1,5 @@
+.filedef temp1 = R16
 .include "src/testprograms/I2Cprotokol.asm"
+.filedef temp2 = R17
+
+mov R18, temp2
