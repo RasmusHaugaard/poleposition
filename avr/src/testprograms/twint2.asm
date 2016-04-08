c7bce1c8d7e953266f749c00b7e8130f499a5042
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2016-04-08 17:21:02 +0200

    interrupts + status in sram

--- avr/src/testprograms/twint2.asm
+++ avr/src/testprograms/twint2.asm
@@ -3,13 +3,17 @@
 .filedef I2CSR = R17
 .filedef temp1 = R16
 
-.equ I2C_TWIE = 0 ;enable/disable twi interrupts
-.equ DEBUG = 1
+.equ I2C_TWIE = 1 ;enable/disable twi interrupts
+.equ DEBUG = 0
 .equ CONTINUOS_STREAM = 0
 
-.equ	acc_addr_w = 0b00111000 ;Adresse til acc for at skrive til den. SDO = GND
-.equ	acc_addr_r = 0b00111001 ;Adresse til acc for at læse fra den. SDO = GND
-.equ	acc_reg_x = 0x29 ;Register adresse for x-værdi
+.equ acc_addr_w = 0b00111000 ;Adresse til acc for at skrive til den. SDO = GND
+.equ acc_addr_r = 0b00111001 ;Adresse til acc for at læse fra den. SDO = GND
+.equ acc_reg_x = 0x29 ;Register adresse for x-værdi
+.equ acc_reg_y = 0x2B
+.equ acc_reg_z = 0x2D
+
+.equ I2CSR_DATA_ADDRESS = 0x60
 
 .equ I2CSR_START = 0
 .equ I2CSR_SADW = 1
@@ -53,6 +57,7 @@ init:
 .endif
 
 	ldi I2CSR, 0
+	sts I2CSR_DATA_ADDRESS, I2CSR
 	rjmp main
 
 main:
@@ -65,6 +70,7 @@ twint_handler:
 	reti
 
 I2C_next:
+	lds I2CSR, I2CSR_DATA_ADDRESS
 .if DEBUG = 1
 	force_send_bt_byte [PS_I2CNEXT]
 	force_send_bt_byte [I2CSR]
@@ -73,48 +79,55 @@ I2C_next:
 	brne not_I2CSR_START
 	rcall I2C_START
 	inc I2CSR
+	sts I2CSR_DATA_ADDRESS, I2CSR
 	ret
 not_I2CSR_START:
 	cpi I2CSR, I2CSR_SADW
 	brne not_I2CSR_SADW
 	rcall I2C_SADW
 	inc I2CSR
+	sts I2CSR_DATA_ADDRESS, I2CSR
 	ret
 not_I2CSR_SADW:
 	cpi I2CSR, I2CSR_SUBR
 	brne not_I2CSR_SUBR
 	rcall I2C_SUBR
 	inc I2CSR
+	sts I2CSR_DATA_ADDRESS, I2CSR
 	ret
 not_I2CSR_SUBR:
 	cpi I2CSR, I2CSR_RESTART
 	brne not_I2CSR_RESTART
 	rcall I2C_RESTART
 	inc I2CSR
+	sts I2CSR_DATA_ADDRESS, I2CSR
 	ret
 not_I2CSR_RESTART:
 	cpi I2CSR, I2CSR_SADR
 	brne not_I2CSR_SADR
 	rcall I2C_SADR
 	inc I2CSR
+	sts I2CSR_DATA_ADDRESS, I2CSR
 	ret
 not_I2CSR_SADR:
 	cpi I2CSR, I2CSR_DATA
 	brne not_I2CSR_DATA
 	rcall I2C_DATA
 	inc I2CSR
+	sts I2CSR_DATA_ADDRESS, I2CSR
 	ret
 not_I2CSR_DATA:
 	cpi I2CSR, I2CSR_STOP
 	brne not_I2CSR_STOP
 	rcall I2C_RECEIVE
-	ldi I2CSR, 0
 .if CONTINUOS_STREAM = 1
 	rcall I2C_START
-	inc I2CSR
+	ldi I2CSR, I2CSR_SADW
 .else
 	rcall I2C_STOP
+	ldi I2CSR, I2CSR_START
 .endif
+	sts I2CSR_DATA_ADDRESS, I2CSR
 	ret
 not_I2CSR_STOP:
 	force_send_bt_byte [ERR_I2CSR_OVERFLOW]
