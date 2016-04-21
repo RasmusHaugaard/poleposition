99e64c5741bc60399db565eef0f7940ca939361e
Phillip M. Kyndbøl <ninka1005@gmail.com>
2016-04-21 15:20:04 +0200

    mini update

--- avr/src/programs/lap_timer_v2.1.asm
+++ avr/src/programs/lap_timer_v2.1.asm
@@ -2,35 +2,38 @@
 ;========== Initalisering ==========
 ;===================================
 
-.equ TCNT1HH = addr
-.set addr = addr + 1					;Opbevare værdi når timer1 overflower (24-bit register) <------------------------- skal have en lokation i sram
+.equ TCNT1HH = addr				;Opbevare værdi når timer1 overflower (24-bit register)
+.set addr = addr + 1					
 
 ;===========================
 ;========== Macro ==========
 ;===========================
 
-.macro get_time_full			;macro som retunere TCNT1HH, TCNT1H og TCNT1L.
-retry_get_time:
-	;tjek overflow
+.macro get_time_full_8_8_8		;macro som retunere TCNT1HH, TCNT1H og TCNT1L.
+	in R16, TIFR				;tjek overflow
+	sbrs R16, TOV1				;skipper hvis TOV1 er sat
+	rjmp TIME_GET
+	rjmp CLEAR_TOV1				;clear TOV1 og inkrimentere TCNT1HH
+TIME_GET:
 	lds R16, TCNT1HH			
-	in R17, TCNT1L
-	in R18, TCNT1H
+	in R17, TCNT1L				;Henter TCNT1L og TCNT1H
+	in R18, TCNT1H				;..
 	lds R19, TCNT1HH			;Tjekker om TCNT1HH har ændret værdi i mellemtiden
 	cpse R16, R19				;..
-	rjmp retry_get_time			;Hvis TCNT1HH har ændret værdi, hentes tid igen
-	mov	@0, R19					;HH
-	mov	@1, R18					;H
-	mov @2,	R17					;L
+	rjmp TIME_GET				;Hvis TCNT1HH har ændret værdi, hentes tid igen
+	mov	@0, R19					;retunere HH
+	mov	@1, R18					;retunere H
+	mov @2,	R17					;retunere L
 .endmacro
 
 
-.macro get_time_hl				;macro som retunere TCNT1H og TCNT1L
+.macro get_time_hl_8_8			;macro som retunere TCNT1H og TCNT1L
 	in @1, TCNT1L
 	in @0, TCNT1H
 .endmacro
 
 
-.macro get_time_hh				;macro som retunere TCNT1HH
+.macro get_time_hh_8			;macro som retunere TCNT1HH
 	lds @0, TCNT1HH
 .endmacro
 
@@ -39,14 +42,14 @@ retry_get_time:
 ;=========================
 
 T1_OV_ISR:						;Interrupt(timer1 overflow)
-	rcall Clear_TOV1
+	rcall CLEAR_TOV1
 	reti						;retunere fra ISR
-
-Clear_TOV1:						;inkrimentere HH og clear overflow flag
+CLEAR_TOV1:						;inkrimentere HH og clear overflow flag
 	lds R16, TCNT1HH			;kopier værdien fra TCNT1HH til R16
 	inc R16						;R16 ++ (ligger 1 til R16)
 	sts TCNT1HH, R16			;Kopier værdi fra R16 Til TCNT1HH
-	;sidde overflow lavt		;clear overflow flag
+	ldi R16, 1<<TOV1			;clear overflow flag
+	out TIFR, R16				;..
 	ret							;retunere
 	
 
--- avr/src/programs/physical_speed.asm
+++ avr/src/programs/physical_speed.asm
@@ -18,8 +18,13 @@ sts last_time_l, R16			;..
 ;========== Macro ==========
 ;===========================
 
-.macro phys_speed				;retunere hastighed
-	;Body       <---------------------------------------------------------------------------------Skriv makro til hastighed
+.macro phys_speed_8_8		;Retunere tid mellem motor tiks
+	ldi R16, 0<<INT1		;Disabler interrupt ved externt trigger 1 (Port D, pin 3)
+	out GICR, R16			;..
+	lds @0, dif_time_h		;..
+	lds @1, dif_time_l		;..
+	ldi R16, 1<<INT1		;tilader interrupt ved externt trigger 1 (Port D, pin 3)
+	out GICR, R16			;..
 .endmacro
 
 ;=========================
@@ -27,15 +32,15 @@ sts last_time_l, R16			;..
 ;=========================
 
 EX1_ISR:					;interrupt(motor tick)
-	get_time_hl_[R18]_[R19]	;kopier "nye timer" til R18 og R19
+	get_time_hl [R18, R19]	;kopier "nye timer" til R18 og R19
 	lds R16, old_time_h		;koper "old timer" fra adresse til R16 og R17
 	lds R17, old_time_l		;..
 	sts old_time_h, R18		;kopier "nye timer" ind i "old time" (som referance ved næste interrupt)
 	sts old_time_l, R19		;..
-	sub R18, R16			;(High bite) Trækker "old timer" (R16) fra den nye "nye timer" (R18)
 	sub R19, R17			;(low bite) Trækker "old timer" (R17) fra den nye "nye timer" (R19)
-	send_bt_byte[R18]		;sender high bite til pc
-	send_bt_byte[R19]		;sender low bite til pc
+	sbc R18, R16			;(High bite) Trækker "old timer" (R16) fra den nye "nye timer" (R18)
+	send_bt_byte [R19]		;sender low bite til pc
+	send_bt_byte [R18]		;sender high bite til pc
 	sts dif_time_h, R18		;gemmer forskellen mellem "ny" og "old" timer
 	sts dif_time_l, R19		;..
 	reti					;retunere fra interrupt
