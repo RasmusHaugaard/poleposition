44b6455ac4367bb6b4ec388d1c545ee23cc02566
Phillip M. Kyndbøl <ninka1005@gmail.com>
2016-04-29 15:15:35 +0200

    mindre update

--- avr/src/mread/map_reading.asm
+++ avr/src/mread/map_reading.asm
@@ -16,15 +16,18 @@
 ;========== Main ==========
 ;==========================
 
-	;disable lap interrupt
-	setspeed [32]				;finder målstreg/startpunkt
-find_finish_line:
-	nop							;venter
-	sbis						;skib hvis externt interrupt flag er sat
-	rjmp find_finish_line		;scanner igen
+	ldi R16, 0<<TOV1			;stopper interrupt ved timer1 overflow
+	out TIMSK, R16				;..
+	setspeed [20]				;finder målstreg/startpunkt
+line_scan:
+	sbis TIFR, TOV1				;skib hvis externt interrupt flag er sat			<------------------------------------- skal tjekke om denne linje bliver brugt rigtigt
+	rjmp line_scan				;scanner igen
 	setspeed [0]				;stopper ved målstreg
-	;clear externt interrupt flag
-	;enable lap interrupt
+	;evt brems					;tænder elektromagneten
+	rjmp T1_OV_ISR_CLEAR		;clear externt interrupt flag
+	;load første sekment
+	ldi R16, 1<<TOV1			;tilader interrupt ved timer1 overflow
+	out TIMSK, R16				;.. 
 	;KØR! MuKØR!!
 
 ;=========================
