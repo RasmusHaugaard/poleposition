7e1f46e724364ddd0e1bcd443fdcb67f8b76eb23
alexdupond <alexdupond@live.dk>
2016-05-11 14:25:32 +0200

    Laptimer

--- avr/src/lapt/lapt.asm
+++ avr/src/lapt/lapt.asm
@@ -6,6 +6,7 @@
 
 .org 0x06		;adresse for extern interrupt 2 (Port B, pin 2)
 jmp EX2_ISR		;adresse med mere plads
+
 .org 0x12		;adresse for timer1 overflow (interrupt vektor table)
 jmp T1_OV_ISR	;adresse med mere plads
 
