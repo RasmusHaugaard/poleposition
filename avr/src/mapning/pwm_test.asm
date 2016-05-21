0bdf6fd4cacae10919ecb7a9efcc26e730632960
alexdupond <alexdupond@live.dk>
2016-05-21 12:05:41 +0200

    Test program til omgang

--- avr/src/mapning/pwm_test.asm
+++ avr/src/mapning/pwm_test.asm
@@ -5,21 +5,29 @@ rjmp init
 
 .org 0x2A
 init:
-
+delays [1]
   .include "src/motor/motor_pwm.asm"
+
+  sbi DDRA, PORTA1
+  nop
+  sbi DDRA, PORTA0
+  nop
+  cbi PORTA, PORTA1
+  nop
+  cbi PORTA, PORTA0
+  nop
+  sbi DDRB, PORTB3
+  nop
+  cbi DDRB, PORTB3
+
 
 main:
 
-  setspeed [160]
+  setspeed [170]
+
+  delays [4]
+
+  setspeed [0]
 
   delays [2]
-
-  setspeed [180]
-
-  delays [2]
-
-  setspeed [200]
-
-  delays [2]
-
   rjmp main
